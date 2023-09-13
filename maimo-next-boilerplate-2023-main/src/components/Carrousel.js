8351c1df72ba130d66e1f3c07801d07b9c43d67f
Luciano Castro <lfcastro@teco.com.ar>
2023-09-13 10:02:25 -0300

    heroCarrussel

--- maimo-next-boilerplate-2023-main/src/components/Carrousel.js
+++ maimo-next-boilerplate-2023-main/src/components/Carrousel.js
@@ -5,11 +5,25 @@ import ShowCard from "./ShowCard";
 export const Carrousel = ({ showsArray }) => {
   return (
     <Swiper
-      slidesPerView={5}
-      spaceBetween={25}
+      slidesPerView={1}
+      spaceBetween={20}
       autoplay={{
         delay: 2500,
         disableOnInteraction: false,
+      }}
+      breakpoints={{
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
       }}
       navigation={true}
       modules={[Pagination, Navigation, Autoplay]}
